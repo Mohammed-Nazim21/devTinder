2f020fb31e58c1075ecc46118534ac895440549f
Nazim Shaikh <nazimshaikh405@gmail.com>
2024-10-05 22:39:49 +0530

    Delete and Update API

--- src/app.js
+++ src/app.js
@@ -20,25 +20,10 @@ app.use(express.json());
 //   }
 // });
 
-// app.get("/user", async (req, res) => {
-//   const userEmail = req.body.emailId;
-//   try {
-//     const user = await User.findOne({ emailId: userEmail });
-//     if (!user) {
-//       res.status(404).send("User not Found");
-//     } else {
-//       res.send(user);
-//     }
-//   } catch (err) {
-//     res.status(400).send("Something Went Wrong");
-//   }
-// });
-
 app.get("/user", async (req, res) => {
-  const userId = req.body.id;
-  console.log(userId);
+  const userEmail = req.body.emailId;
   try {
-    const user = await User.findById(userId);
+    const user = await User.findOne({ emailId: userEmail });
     if (!user) {
       res.status(404).send("User not Found");
     } else {
@@ -48,6 +33,21 @@ app.get("/user", async (req, res) => {
     res.status(400).send("Something Went Wrong");
   }
 });
+
+// app.get("/user", async (req, res) => {
+//   const userId = req.body.id;
+//   console.log(userId);
+//   try {
+//     const user = await User.findById(userId);
+//     if (!user) {
+//       res.status(404).send("User not Found");
+//     } else {
+//       res.send(user);
+//     }
+//   } catch (err) {
+//     res.status(400).send("Something Went Wrong");
+//   }
+// });
 
 // Feed API - GET/feed - get all users from the database
 app.get("/feed", async (req, res) => {
@@ -71,6 +71,37 @@ app.post("/signup", async (req, res) => {
     res.send("User Added successfully!");
   } catch (err) {
     res.status(400).send("Error saving the user:" + err.message);
+  }
+});
+
+// Delete user from database
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User Deleted");
+  } catch (err) {
+    res.status(400).send("Something Went Wrong");
+  }
+});
+
+// Update data of the user
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  // const userEmaiId = req.body.emailId;
+  const data = req.body;
+  try {
+    // const user = await User.findByIdAndUpdate(userId, data, {
+    //   returnDocument: "after",
+    // });
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+    });
+    // const user = await User.findOneAndUpdate({ emailId: userEmaiId }, data);
+    console.log(user);
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("Something Went Wrong");
   }
 });
 
