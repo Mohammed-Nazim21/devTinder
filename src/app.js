7ea1eb3187249117490f4f52e5b510c03b55da6e
Nazim Shaikh <nazimshaikh405@gmail.com>
2024-10-05 17:37:23 +0530

    GET apis using model.find()

--- src/app.js
+++ src/app.js
@@ -5,6 +5,64 @@ const User = require("./models/user");
 
 app.use(express.json());
 
+// GET user by email
+// app.get("/user", async (req, res) => {
+//   const userEmail = req.body.emailId;
+//   try {
+//     const users = await User.find({ emailId: userEmail });
+//     if (users.length === 0) {
+//       res.status(404).send("User not Found");
+//     } else {
+//       res.send(users);
+//     }
+//   } catch (err) {
+//     res.status(400).send("Something Went Wrong");
+//   }
+// });
+
+// app.get("/user", async (req, res) => {
+//   const userEmail = req.body.emailId;
+//   try {
+//     const user = await User.findOne({ emailId: userEmail });
+//     if (!user) {
+//       res.status(404).send("User not Found");
+//     } else {
+//       res.send(user);
+//     }
+//   } catch (err) {
+//     res.status(400).send("Something Went Wrong");
+//   }
+// });
+
+app.get("/user", async (req, res) => {
+  const userId = req.body.id;
+  console.log(userId);
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      res.status(404).send("User not Found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something Went Wrong");
+  }
+});
+
+// Feed API - GET/feed - get all users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("No Users Found.");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something Went Wrong");
+  }
+});
+
 app.post("/signup", async (req, res) => {
   // Creating a new instance of the User model
   const user = new User(req.body);
